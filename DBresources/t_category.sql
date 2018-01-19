9c3e2744a5e0f9e8bae6cad0d6d5ffda866ef7af
Neal <2656798470@qq.com>
2018-01-19 10:38:53 +0800

    数据库测试数据 commit

---
+++ DBresources/t_category.sql
@@ -0,0 +1,6 @@
+INSERT INTO `t_category` VALUES (1, '编程语言');
+INSERT INTO `t_category` VALUES (2, '框架技术');
+INSERT INTO `t_category` VALUES (3, '数据库');
+INSERT INTO `t_category` VALUES (4, '数据结构、算法');
+INSERT INTO `t_category` VALUES (5, '环境搭建');
+INSERT INTO `t_category` VALUES (6, '其他');
